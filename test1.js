c297eef0b979330fe9423f996ae2fef4eb761cb2
snrnapa <shino.satoru@gmail.com>
2024-02-12 09:41:58 +0900

    ひな形を作成した

---
+++ test1.js
@@ -0,0 +1,16 @@
+const excuteCount = 100;
+
+const testArray = [
+  "トマト",
+  "キャベツ",
+  "パプリカ",
+  "ミニトマト",
+  "ジャガイモ",
+  "玉ねぎ",
+];
+
+console.log(testArray[3]);
+
+// for (let i = 0; i < excuteCount; i++) {
+//   console.log("hello world" + i);
+// }
